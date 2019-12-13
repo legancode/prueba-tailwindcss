c717da444fdff872106154f2f33b4496aba4dc7a
Angel Salvador <a.salvador@2wbtl.com>
2019-12-13 17:27:26 -0500

    init

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  theme: {
+    colors: {
+      primary: "#E3C4A8",
+      gray: "#F8F9FA",
+      white: "#fff",
+      dark: "#333333"
+    },
+    fontFamily: {
+      display: ["Oswald", "sans-serif"],
+      body: ['"Nunito Sans"', "sans-serif"]
+    },
+    fontSize: {
+      xs: "0.8rem",
+      base: "1rem",
+      lg: "1.5rem",
+      xl: "d",
+      "2xl": "4rem"
+    }
+  },
+  variants: {},
+  plugins: []
+};
